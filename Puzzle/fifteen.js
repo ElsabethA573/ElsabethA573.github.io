7ef4e7196de92c251ff12349d474590e635253d0
ElsabethA573 <eabreha@mui.edu>
2020-06-05 13:20:03 -0500

    git add .

---
+++ Puzzle/fifteen.js
@@ -0,0 +1,25 @@
+$("body, button").css({
+	"font-family": "cursive",
+	"font-size": "14pt",
+	"text-align": "center"
+    });
+   
+    $("#puzzlearea").children().css({
+        "float":"left",
+        "border":"5px solid black",
+        "height":"90px",
+        "width":"90px",
+       
+        //,"position": "absolute"
+    });
+    $("#puzzlearea").css({
+        "font-size": "40pt",
+	"width": "400px",
+	"height":"400px",
+	"margin-left": "auto",
+	"margin-right": "auto",
+    "position": "relative"
+    ,"background-image": "url(background.jpg)"
+
+
+     });
